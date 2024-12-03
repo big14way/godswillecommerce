396f1d3763b43a3f4fdca98ca094638187bd52c3
big14way <idolorwilly@icloud.com>
2024-12-03 14:01:17 +0100

    change

---
+++ admin/src/App.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Add from './pages/Add';
+import List from './pages/List';
+import Orders from './pages/Orders';
+import Navbar from './components/Navbar';
+import SideBar from './components/Sidebar';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+const App = () => {
+  const [token, setToken] = useState(localStorage.getItem('token') || '');
+
+  useEffect(() => {
+    localStorage.setItem('token', token);
+  }, [token]);
+
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      <ToastContainer />
+      {token === "" ? (
+        <Login setToken={setToken} />
+      ) : (
+        <>
+          <Navbar setToken={setToken} />
+          <hr />
+          <div className="flex min-h-screen">
+            <SideBar />
+          </div>
+          <div className="flex-grow w-full mx-auto my-8 text-gray-600 text-base">
+            <Routes>
+              <Route path="/add" element={<Add token={token} />} />
+              <Route path="/list" element={<List token={token} />} />
+              <Route path="/orders" element={<Orders token={token} />} />
+            </Routes>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ admin/src/pages/Add.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import {assets} from '../assets/assets'
+
+const Add = () => {
+  return (
+    <form className='flex flex-col w-full items-start gap-3'>
+        <div>
+          <p className='mb-2'>Upload Image</p>
+
+          <div className='flex gap-2'>
+            <label htmlFor="image1">
+            <img className='w-20' src={assets.upload_area} alt="" />
+            <input type="file" id="image1" hidden/>
+            </label>
+            <label htmlFor="image2">
+            <img className='w-20' src={assets.upload_area} alt="" />
+            <input type="file" id="image2" hidden/>
+            </label>
+            <label htmlFor="image3">
+            <img className='w-20' src={assets.upload_area} alt="" />
+            <input type="file" id="image3" hidden/>
+            </label>
+            <label htmlFor="image4">
+            <img className='w-20' src={assets.upload_area} alt="" />
+            <input type="file" id="image4" hidden/>
+            </label>
+          </div>
+        </div>
+
+        <div className='w-full'>
+          <p className='mb-2'>Product name</p>
+          <input className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Type here' required/>
+        </div>
+        <div className='w-full'>
+          <p className='mb-2'>Product description</p>
+          <textarea className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='write content here' required/>
+        </div>
+
+        <div>
+          <div>
+            <p>
+              Product category
+            </p>
+            <select>
+              <option value="Men">Men</option>
+              <option value="Women">Women</option>
+              <option value="Kids">Kids</option>
+            </select>
+          </div>
+
+          <div>
+            <p>
+              Sub category
+            </p>
+            <select>
+              <option value="Topwear">Topwear</option>
+              <option value="Bottomwear">Bottomwear</option>
+              <option value="Winterwear">Winterwear</option>
+            </select>
+          </div>
+
+          <div>
+            <p>Product price</p>
+            <input type="Number" placeholder='25' />
+          </div>
+
+        </div>
+
+    </form>
+  );
+};
+
+export default Add;
